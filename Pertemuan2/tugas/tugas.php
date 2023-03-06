b6c6516ae11aac37f814470213c5865d599db9d9
wulanrmdn <wulanrmdnapabungsu04@gmail.com>
2023-03-06 12:36:58 +0700

    new upload

---
+++ Pertemuan2/tugas/tugas.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+
+<form method = "POST" action ="proses_nilai1.php">
+  <div class="form-group row">
+    <label for="nama" class="col-4 col-form-label">nama lengkap</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-address-card"></i>
+          </div>
+        </div> 
+        <input id="nama" name="nama" placeholder="masukan nama" type="text" class="form-control">
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="matkul" class="col-4 col-form-label">mata kuliah</label> 
+    <div class="col-8">
+      <input id="matkul" name="matkul" placeholder="masukan matkul" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="uts" class="col-4 col-form-label">nilai uts</label> 
+    <div class="col-8">
+      <input id="uts" name="uts" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="uas" class="col-4 col-form-label">nilai uas</label> 
+    <div class="col-8">
+      <input id="uas" name="uas" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="praktikum" class="col-4 col-form-label">praktikum</label> 
+    <div class="col-8">
+      <input id="praktikum" name="praktikum" type="text" class="form-control">
+    </div>
+  </div>
+ 
+   
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+</form>
