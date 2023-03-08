fbb7c63bd44d9ec09621b577a3732ea82e247002
wulanrmdn <wulanrmdnapabungsu04@gmail.com>
2023-03-08 11:47:02 +0700

    pertemuan2

--- Pertemuan2/tugas/form_nilai.php
+++ Pertemuan2/tugas/form_nilai.php
@@ -5,55 +5,54 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Nilai Siswa</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-</head>
-<body>
-    <div class="container">
-        <h2>Form Nilai Siswa</h2>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    </head>
+    <body>
+        <h1 style="padding: 10px;">Nilai Form Siswa</h1>
         <hr>
-
-<form method="GET" action="Proses_Nilai.php"> 
-  <div class="form-group row">
-    <label for="nama" class="col-4 col-form-label">Nama</label> 
-    <div class="col-8">
-      <input id="nama" name="nama" placeholder="Masukkan Nama" type="text" class="form-control">
-    </div>
-  </div>
-  <div class="form-group row">
-    <label for="matkul" class="col-4 col-form-label">Mata Kuliah</label> 
-    <div class="col-8">
-      <select id="matkul" name="matkul" class="custom-select">
-        <option value="Web">Pemrograman Web</option>
-        <option value="DDP">Dasar-Dasar Pemmrograman</option>
-        <option value="UI/UX">UI/UX</option>
-      </select>
-    </div>
-  </div>
-  <div class="form-group row">
-    <label for="UTS" class="col-4 col-form-label">Nilai UTS</label> 
-    <div class="col-8">
-      <input id="UTS" name="UTS" placeholder="Masukan Nilai UTS" type="text" class="form-control">
-    </div>
-  </div>
-  <div class="form-group row">
-    <label for="UAS" class="col-4 col-form-label">Nilai UAS</label> 
-    <div class="col-8">
-      <input id="UAS" name="UAS" placeholder="Masukkan Nilai UAS" type="text" class="form-control">
-    </div>
-  </div>
-  <div class="form-group row">
-    <label for="Praktikum" class="col-4 col-form-label">Nilai Praktikum</label> 
-    <div class="col-8">
-      <input id="Praktikum" name="Praktikum" placeholder="Masukkan Nilai Praktikum" type="text" class="form-control">
-    </div>
-  </div> 
-  <div class="form-group row">
-    <div class="offset-4 col-8">
-      <button name="submit" type="submit" class="btn btn-primary">Submit</button>
-    </div>
-  </div>
-</form>
-    </div>
-    
-</body>
-</html> 
+        <div class="container">
+            <form method="POST" action="proses_nilai.php">
+                <div class="form-group row">
+                    <label for="nama" class="col-4 col-form-label">Nama Lengkap</label>
+                    <div class="col-6">
+                        <input id="nama" name="nama" placeholder="Nama Lengkap" type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="matkul" class="col-4 col-form-label">Mata Kuliah</label>
+                    <div class="col-6">
+                        <select id="matkul" name="matkul" class="custom-select">
+                            <option value="WEB">Pemograman Web</option>
+                            <option value="DDP">Dasar Dasar Pemograman</option>
+                            <option value="UIUX">UIUX</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="uts" class="col-4 col-form-label">Nilai UTS</label>
+                    <div class="col-4">
+                        <input id="uts" name="uts" placeholder="Nilai UTS" type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="uas" class="col-4 col-form-label">Nilai UAS</label>
+                    <div class="col-4">
+                        <input id="uas" name="uas" placeholder="Nilai UAS" type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="tugas" class="col-4 col-form-label">Nilai Tugas/Praktikum</label>
+                    <div class="col-4">
+                        <input id="tugas" name="tugas" placeholder="Nilai Tugas" type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="offset-4 col-8">
+                        <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </div>
+            </form>
+            
+        </div>
+    </body>
+</html>
--- Pertemuan2/tugas/proses_nilai.php
+++ Pertemuan2/tugas/proses_nilai.php
@@ -1,17 +1,27 @@
 <?php
-if (isset($_GET["submit"])){
-    $nama = $_GET ["nama"];
-    $matkul = $_GET ["matkul"];
-    $uts = $_GET ["UTS"];
-    $uas = $_GET ["UAS"];
-    $praktikum = $_GET ["Praktikum"];
 
-    echo "Nama: $nama <br>";
-    echo "Mata Kuliah: $matkul <br>";
-    echo "Nilai UTS: $uts <br>";
-    echo "Nilai UAS: $uas <br>";
-    echo "Nilai Praktikum: $praktikum <br>";
-} else {
-    echo "Tidak ada data";
+    $nama = $_POST['nama'];
+    $matkul = $_POST['matkul'];
+    $uts = $_POST['uts'];
+    $uas = $_POST['uas'];
+    $tugas = $_POST['tugas'];
+    $grade = ($uts + $uas + $tugas) / 3;
 
-}
+    echo '<br>Nama : ' . $nama;
+    echo '<br>Mata Kuliah : ' . $matkul;
+    echo '<br>Nilai UTS : ' . $uts;
+    echo '<br>Nilai UAS : ' . $uas;
+    echo '<br>Nilai Tugas : ' . $tugas;
+    echo "<br>Rata-rata : " . round(($uts + $uas + $tugas) / 3);
+    echo("<br>");
+    if ($grade>=85)
+    echo("Grade : <b>A</b>");
+    elseif ($grade>=70)
+    echo("Grade : <b>B</b>");
+    elseif ($grade>=56)
+    echo("Grade : <b>C</b>");
+    elseif ($grade>=36)
+    echo("Grade : <b>D</b>");
+    elseif ($grade>=0)
+    echo("Grade : <b>E</b>");
+?>
