dda047fe95af42433ce35e65428510bc0f917e87
wulanrmdn <wulanrmdnapabungsu04@gmail.com>
2023-04-13 08:16:10 +0700

    Pertemuan7

---
+++ Pertemuan7/Hero.php
@@ -0,0 +1,50 @@
+<?php
+class Hero
+{
+    // Attribut / Properties pada class
+    public $name;
+    public $health;
+    public $damage;
+    public $level = 1;
+
+    // constructor akan dijalankan secara otomatis
+    public function __construct($name, $health, $damage)
+    {
+        $this->name = $name;
+        $this->health = $health;
+        $this->damage = $damage;
+    }
+
+    // method untuk menampilkan informasi hero
+    public function getInfo()
+    {
+        echo "Hero: " . $this->name;
+        echo "<br> HP: " . $this->health;
+        echo "<br> Damage: " . $this->damage;
+        echo "<br> Level: " . $this->level;
+
+    }
+
+    // method untuk naik satu level
+    public function levelUp($number)
+    {
+        $this->level = $this->level + $number;
+        $this->health = $this->health + (200 * $number);
+        $this->damage = $this->damage + (50 * $number);
+    }
+
+
+}
+
+// membuat objek dari class Hero
+$hero1 = new Hero('Alucard', 3200, 200);
+$hero2 = new Hero('Franco', 5000, 50);
+
+// memanggil method
+$hero1->getInfo();
+
+echo "<br> <hr>";
+
+$hero1->levelUp(4);
+
+$hero1->getInfo();
---
+++ Pertemuan7/motor_nmax.php
@@ -0,0 +1,29 @@
+<?php
+
+class Motor
+{
+    public $merk;
+    public $bensin;
+    public $topSpeed;
+    public $cc;
+
+    public function __construct($nama_motor, $merk, $bensin, $topSpeed, $cc)
+    {
+        $this->nama_motor = $nama_motor;
+        $this->merk = $merk;
+        $this->bensin = $bensin;
+        $this->topSpeed = $topSpeed;
+        $this->cc = $cc;
+    }
+
+    public function jalan($jarak)
+    {
+        return "Bensin berkurang sebesar : " . ($jarak / 30) . " liter";
+    }
+}
+
+$motor1 = new Motor('NMax', 'Yamaha',  5, 200, 1000);
+echo "Nama Motor : " . $motor1->nama_motor . "<br/>";
+echo "Merk Motor: " . $motor1->merk . "<br/>";
+echo "Jarak Tempuh: 150km <br/>";
+echo $motor1->jalan(150);
